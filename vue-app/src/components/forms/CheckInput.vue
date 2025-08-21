1ca215dea030077437bbe29e3b6bc66074e385a1
Serhii Kushnir <skushnir@i.ua>
2025-08-21 13:18:57 +0300

    Section 4: Routing with the Vue Router

---
+++ vue-app/src/components/forms/CheckInput.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="form-check">
+          <input class="form-check-input" :checked="checked" :required="required" type="checkbox" :value="value" :id="name" :name="name">
+          <label class="form-check-label" for="name">
+            {{ label }}
+          </label>
+        </div>
+</template>
+
+<script>
+export default {
+  name: 'CheckInput',
+  rops: ["label", "required", "name", "value", "checked"],
+}
+</script>
---
+++ vue-app/src/components/forms/FormTag.vue
@@ -0,0 +1,34 @@
+<template>
+  <form 
+    @submit.prevent="submit"
+    autocomplete="off" 
+    :method="method" 
+    :action="action" 
+    class="needs-validation" 
+    novalidate>
+    <slot></slot>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'FormTag',
+  props: [
+    'method',
+    'action',
+    'event',
+    'name'
+  ],
+  methods: {
+    // Bootstrap validation
+    submit(event) {
+      const myForm = event.target;
+      if (myForm.checkValidity()) {
+        this.$emit(this.event);
+      }
+      myForm.classList.add('was-validated');
+    }
+  },
+}
+  
+</script>
